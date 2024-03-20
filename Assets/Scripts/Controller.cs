87b2e41cc57f5ea8071d7fb689d2baeec646043a
msle237-lees <59542013+msle237-lees@users.noreply.github.com>
2024-03-20 17:54:21 -0400

    starting work on the http connection

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -5,9 +5,12 @@ using UnityEngine.SceneManagement;
 using System.Runtime.InteropServices;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class Controller : MonoBehaviour
 {
+    public bool remoteControl = false;
     private ControllerMapping controllerMapping;
     private float X, Y, Z, Roll, Pitch, Yaw;
     private bool Torp1, Torp2;
@@ -25,6 +28,24 @@ public class Controller : MonoBehaviour
     public GameObject Qualification_Pole;
     public TMPro.TextMeshProUGUI scoreText;
     private int score = 0;
+    private string url = "http://localhost:5000";
+    // HTTP functions
+    IEnumerator getData(string url) {
+        UnityWebRequest uwr = UnityWebRequest.Get(url);
+        yield return uwr.SendWebRequest();
+
+        if (uwr.result == UnityWebRequest.Result.ConnectionError) {
+            Debug.Log("Error While Sending: " + uwr.error);
+        }
+        else {
+            Debug.Log("Received: " + uwr.downloadHandler.text);
+        }
+    }
+    private void postData(string url, string sensor_data, int[][] img1, int[][] img2, int[][] img3) {
+        // Create a posting method to send three images to the server
+        // UnityWebRequest uwr = new UnityWebRequest(url, "POST");
+        // byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(sensor_data);
+    }
     void Start()
     {
         // Initialize ControllerMapping
@@ -49,63 +70,68 @@ public class Controller : MonoBehaviour
         coin_7.SetActive(false);
         coin_8.SetActive(false);
     }
+    private void OnTriggerEnter(Collider other) {
+        if (Time.time - lastSwitchTime > debounceTime) {
+            if (other.gameObject == coin_0) {
+                coin_0.SetActive(false);
+                coin_1.SetActive(true);
+                score += 1;
+                scoreText.text = score.ToString();
+            }
+            else if (other.gameObject == coin_1) {
+                coin_1.SetActive(false);
+                coin_2.SetActive(true);
+                score += 1;
+                scoreText.text = score.ToString();
+            }
+            else if (other.gameObject == coin_2) {
+                coin_2.SetActive(false);
+                coin_3.SetActive(true);
+                score += 1;
+                scoreText.text = score.ToString();
+            }
+            else if (other.gameObject == coin_3) {
+                coin_3.SetActive(false);
+                coin_4.SetActive(true);
+                score += 1;
+                scoreText.text = score.ToString();
+            }
+            else if (other.gameObject == coin_4) {
+                coin_4.SetActive(false);
+                coin_5.SetActive(true);
+                score += 1;
+                scoreText.text = score.ToString();
+            }
+            else if (other.gameObject == coin_5) {
+                coin_5.SetActive(false);
+                coin_6.SetActive(true);
+                score += 1;
+                scoreText.text = score.ToString();
+            }
+            else if (other.gameObject == coin_6) {
+                coin_6.SetActive(false);
+                coin_7.SetActive(true);
+                score += 1;
+                scoreText.text = score.ToString();
+            }
+            else if (other.gameObject == coin_7) {
+                coin_7.SetActive(false);
+                coin_8.SetActive(true);
+                score += 1;
+                scoreText.text = score.ToString();
+            }
+            else if (other.gameObject == coin_8) {
+                coin_8.SetActive(false);
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
     void OnCollisionEnter(Collision collision)
     {   
         if (collision.gameObject == gate || collision.gameObject == Qualification_Pole) {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
-        else if (collision.gameObject == coin_0) {
-            coin_0.SetActive(false);
-            coin_1.SetActive(true);
-            score += 1;
-            scoreText.text = score.ToString();
-        }
-        else if (collision.gameObject == coin_1) {
-            coin_1.SetActive(false);
-            coin_2.SetActive(true);
-            score += 1;
-            scoreText.text = score.ToString();
-        }
-        else if (collision.gameObject == coin_2) {
-            coin_2.SetActive(false);
-            coin_3.SetActive(true);
-            score += 1;
-            scoreText.text = score.ToString();
-        }
-        else if (collision.gameObject == coin_3) {
-            coin_3.SetActive(false);
-            coin_4.SetActive(true);
-            score += 1;
-            scoreText.text = score.ToString();
-        }
-        else if (collision.gameObject == coin_4) {
-            coin_4.SetActive(false);
-            coin_5.SetActive(true);
-            score += 1;
-            scoreText.text = score.ToString();
-        }
-        else if (collision.gameObject == coin_5) {
-            coin_5.SetActive(false);
-            coin_6.SetActive(true);
-            score += 1;
-            scoreText.text = score.ToString();
-        }
-        else if (collision.gameObject == coin_6) {
-            coin_6.SetActive(false);
-            coin_7.SetActive(true);
-            score += 1;
-            scoreText.text = score.ToString();
-        }
-        else if (collision.gameObject == coin_7) {
-            coin_7.SetActive(false);
-            coin_8.SetActive(true);
-            score += 1;
-            scoreText.text = score.ToString();
-        }
-        else if (collision.gameObject == coin_8) {
-            coin_8.SetActive(false);
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        }
+        
     }
     /**
      * @brief Map a value from one range to another.
@@ -120,107 +146,117 @@ public class Controller : MonoBehaviour
     }
     void FixedUpdate()
     {
-        // Controller inputs
-        float LY = controllerMapping.LY.Newaction.ReadValue<float>();
-        float LX = controllerMapping.LX.Newaction.ReadValue<float>();
-        float RX = controllerMapping.RX.Newaction1.ReadValue<float>();
-        float RY = controllerMapping.RY.Newaction.ReadValue<float>();
-        float RT = controllerMapping.TriggerR.Newaction.ReadValue<float>();
-        float LT = controllerMapping.TriggerL.Newaction.ReadValue<float>();
-        bool A = controllerMapping.A.Newaction.ReadValue<float>() > 0;
-        bool B = controllerMapping.B.Newaction.ReadValue<float>() > 0;
-        bool Xb = controllerMapping.X.Newaction.ReadValue<float>() > 0;
-        bool Yb = controllerMapping.Y.Newaction.ReadValue<float>() > 0;
-        bool DPadUp = controllerMapping.DPadU.Newaction.ReadValue<float>() > 0;
-        bool DPadDown = controllerMapping.DPadD.Newaction.ReadValue<float>() > 0;
-        bool DPadLeft = controllerMapping.DPadL.Newaction.ReadValue<float>() > 0;
-        bool DPadRight = controllerMapping.DPadR.Newaction.ReadValue<float>() > 0;
+        if (!remoteControl) {
+            // Controller inputs
+            float LY = controllerMapping.LY.Newaction.ReadValue<float>();
+            float LX = controllerMapping.LX.Newaction.ReadValue<float>();
+            float RX = controllerMapping.RX.Newaction1.ReadValue<float>();
+            float RY = controllerMapping.RY.Newaction.ReadValue<float>();
+            float RT = controllerMapping.TriggerR.Newaction.ReadValue<float>();
+            float LT = controllerMapping.TriggerL.Newaction.ReadValue<float>();
+            bool A = controllerMapping.A.Newaction.ReadValue<float>() > 0;
+            bool B = controllerMapping.B.Newaction.ReadValue<float>() > 0;
+            bool Xb = controllerMapping.X.Newaction.ReadValue<float>() > 0;
+            bool Yb = controllerMapping.Y.Newaction.ReadValue<float>() > 0;
+            bool DPadUp = controllerMapping.DPadU.Newaction.ReadValue<float>() > 0;
+            bool DPadDown = controllerMapping.DPadD.Newaction.ReadValue<float>() > 0;
+            bool DPadLeft = controllerMapping.DPadL.Newaction.ReadValue<float>() > 0;
+            bool DPadRight = controllerMapping.DPadR.Newaction.ReadValue<float>() > 0;
 
-        // Adjusted movement inputs
-        X = -RY;
-        Y = -LX;
-        Z = LY;
-        Yaw = -RX;
-        Torp1 = RT > 0.0f;
-        Torp2 = LT > 0.0f;
+            // Adjusted movement inputs
+            X = -LY;
+            Y = -LX;
+            Z = RY;
+            Yaw = -RX;
+            Torp1 = RT > 0.0f;
+            Torp2 = LT > 0.0f;
 
-        // Mapping input values to a specific range for movement
-        float min_out = -10.0f;
-        float max_out = 10.0f;
-        float min_Z_in = -25.0f;
-        float max_Z_in = 25.0f;
-        float min_1_out = -0.75f;
-        float max_1_out = 0.75f;
-        float min_in = -1.0f;
-        float max_in = 1.0f;
+            // Mapping input values to a specific range for movement
+            float min_out = -10.0f;
+            float max_out = 10.0f;
+            float min_Z_in = -25.0f;
+            float max_Z_in = 25.0f;
+            float min_1_out = -0.75f;
+            float max_1_out = 0.75f;
+            float min_in = -1.0f;
+            float max_in = 1.0f;
 
-        // Apply input mapping
-        X = Map(X, min_in, max_in, min_out, max_out);
-        Y = Map(Y, min_in, max_in, min_out, max_out);
-        Z = Map(Z, min_in, max_in, min_Z_in, max_Z_in);
-        Yaw = Map(Yaw, min_in, max_in, min_1_out, max_1_out);
+            // Apply input mapping
+            X = Map(X, min_in, max_in, min_out, max_out);
+            Y = Map(Y, min_in, max_in, min_out, max_out);
+            Z = Map(Z, min_in, max_in, min_Z_in, max_Z_in);
+            Yaw = Map(Yaw, min_in, max_in, min_1_out, max_1_out);
 
-        Yaw = -Yaw; // Invert Yaw
+            Yaw = -Yaw; // Invert Yaw
 
-        // Direction and magnitude of force to apply, in local space
-        Vector3 forceDirection = new Vector3(X, Z, Y);
-        Vector3 torqueDirection = new Vector3(0, Yaw, 0); // Adjust for local space orientation
-
-        // Convert forceDirection and torqueDirection to be relative to the rigidbody's orientation
-        Vector3 localForce = rb.transform.TransformDirection(forceDirection);
-        Vector3 localTorque = rb.transform.TransformDirection(torqueDirection);
-
-        // Apply forces and torques to the rigidbody in local space
-        rb.AddForce(localForce, ForceMode.Force);
-        rb.AddTorque(localTorque, ForceMode.Force);
-
-        // Debug output
-        Debug.Log($"X: {X} Y: {Y} Z: {Z} Roll: {Roll} Pitch: {Pitch} Yaw: {Yaw} Torp1: {Torp1} Torp2: {Torp2} A: {A} B: {B} X: {Xb} Y: {Yb} DPadUp: {DPadUp} DPadDown: {DPadDown} localForce: {localForce} localTorque: {localTorque} cameraIndex: {cameraIndex}");
-
-        if (Time.time - lastSwitchTime > debounceTime) {
-            if (DPadUp) {
-                cameraIndex += 1;
-                lastSwitchTime = Time.time;
+            // Add a ceiling to the Y axis based on player location
+            if (rb.transform.position.y > 10.0f) {
+                Z = Mathf.Min(Z, 0.0f);
             }
-            else if (DPadDown) {
-                cameraIndex -= 1;
-                lastSwitchTime = Time.time;
+
+            // Direction and magnitude of force to apply, in local space
+            Vector3 forceDirection = new Vector3(X, Z, Y);
+            Vector3 torqueDirection = new Vector3(0, Yaw, 0); // Adjust for local space orientation
+
+            // Convert forceDirection and torqueDirection to be relative to the rigidbody's orientation
+            Vector3 localForce = rb.transform.TransformDirection(forceDirection);
+            Vector3 localTorque = rb.transform.TransformDirection(torqueDirection);
+
+            // Apply forces and torques to the rigidbody in local space
+            rb.AddForce(localForce, ForceMode.Force);
+            rb.AddTorque(localTorque, ForceMode.Force);
+
+            // Debug output
+            Debug.Log($"X: {X} Y: {Y} Z: {Z} Roll: {Roll} Pitch: {Pitch} Yaw: {Yaw} Torp1: {Torp1} Torp2: {Torp2} A: {A} B: {B} X: {Xb} Y: {Yb} DPadUp: {DPadUp} DPadDown: {DPadDown} localForce: {localForce} localTorque: {localTorque} cameraIndex: {cameraIndex}");
+
+            if (Time.time - lastSwitchTime > debounceTime) {
+                if (DPadUp) {
+                    cameraIndex += 1;
+                    lastSwitchTime = Time.time;
+                }
+                else if (DPadDown) {
+                    cameraIndex -= 1;
+                    lastSwitchTime = Time.time;
+                }
             }
-        }
-        if (cameraIndex < 0) {
-            cameraIndex = 0;
-        }
-        else if (cameraIndex > 3) {
-            cameraIndex = 3;
-        }
-        if (cameraIndex == 0) {
-            frontRightCamera.enabled = false;
-            frontLeftCamera.enabled = false;
-            bottomCamera.enabled = false;
-            sceneCamera.enabled = true;
-        }
-        else if (cameraIndex == 1) {
-            frontRightCamera.enabled = true;
-            frontLeftCamera.enabled = false;
-            bottomCamera.enabled = false;
-            sceneCamera.enabled = false;
-        }
-        else if (cameraIndex == 2) {
-            frontRightCamera.enabled = false;
-            frontLeftCamera.enabled = true;
-            bottomCamera.enabled = false;
-            sceneCamera.enabled = false;
-        }
-        else if (cameraIndex == 3) {
-            frontRightCamera.enabled = false;
-            frontLeftCamera.enabled = false;
-            bottomCamera.enabled = true;
-            sceneCamera.enabled = false;        
-        }
-        if (Time.time - lastSwitchTime > debounceTime) {
-            if (Yb) {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            if (cameraIndex < 0) {
+                cameraIndex = 0;
             }
+            else if (cameraIndex > 3) {
+                cameraIndex = 3;
+            }
+            if (cameraIndex == 0) {
+                frontRightCamera.enabled = false;
+                frontLeftCamera.enabled = false;
+                bottomCamera.enabled = false;
+                sceneCamera.enabled = true;
+            }
+            else if (cameraIndex == 1) {
+                frontRightCamera.enabled = true;
+                frontLeftCamera.enabled = false;
+                bottomCamera.enabled = false;
+                sceneCamera.enabled = false;
+            }
+            else if (cameraIndex == 2) {
+                frontRightCamera.enabled = false;
+                frontLeftCamera.enabled = true;
+                bottomCamera.enabled = false;
+                sceneCamera.enabled = false;
+            }
+            else if (cameraIndex == 3) {
+                frontRightCamera.enabled = false;
+                frontLeftCamera.enabled = false;
+                bottomCamera.enabled = true;
+                sceneCamera.enabled = false;        
+            }
+            if (Time.time - lastSwitchTime > debounceTime) {
+                if (Yb) {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                }
+            }
+        } else {
+            // Placeholder for remote control
+            // StartCoroutine(getData(url));
         }
     }
 
